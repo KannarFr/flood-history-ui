2f9a3b8acd385a5db279b0a3e85007734d2321fe
Alexandre DUVAL <alexanduval@wanadoo.fr>
2019-07-10 18:27:01 +0200

    npm fix audit and redirect https force

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HttpsRedirect from 'react-https-redirect';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HttpsRedirect><App /></HttpsRedirect>, document.getElementById('root'));
 registerServiceWorker();
